699873319354916398053f96e44a7065c1fa2591
balajiamgitup <104089954+balajiamgitup@users.noreply.github.com>
2022-09-07 02:59:41 -0700

    Add files via upload

---
+++ Bootstrap/EmployeeProject/src/data.js
@@ -0,0 +1,42 @@
+var myList=[{"Employee Id" : 123456, "Name" : "Dinga","Projet": "DataBase","Salary":250000,"Location":"TVM"},
+            {"Employee Id" : 123457, "Name" : "Dingi","Projet": "FrontEnd","Salary":450000,"Location":"Kochi"},
+            {"Employee Id" : 123458, "Name" : "Mr.Bean","Projet": "TSG","Salary":750000,"Location":"Banglore"}];
+
+// Builds the HTML Table out of myList json data from Ivy restful service.
+ function buildHtmlTable() {
+     var columns = addAllColumnHeaders(myList);
+ 
+     for (var i = 0 ; i < myList.length ; i++) {
+         var row$ = $('<tr/>');
+         for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+             var cellValue = myList[i][columns[colIndex]];
+ 
+             if (cellValue == null) { cellValue = ""; }
+ 
+             row$.append($('<td/>').html(cellValue));
+         }
+         $("#excelDataTable").append(row$);
+     }
+ }
+ 
+ // Adds a header row to the table and returns the set of columns.
+ // Need to do union of keys from all records as some records may not contain
+ // all records
+ function addAllColumnHeaders(myList)
+ {
+     var columnSet = [];
+     var headerTr$ = $('<tr/>');
+ 
+     for (var i = 0 ; i < myList.length ; i++) {
+         var rowHash = myList[i];
+         for (var key in rowHash) {
+             if ($.inArray(key, columnSet) == -1){
+                 columnSet.push(key);
+                 headerTr$.append($('<th/>').html(key));
+             }
+         }
+     }
+     $("#excelDataTable").append(headerTr$);
+ 
+     return columnSet;
+ }
